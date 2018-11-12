ea59501b7e3a37776cbe8ad5a274db271484d7b5
mlwhitmer <whitmerlm@gmail.com>
2018-11-12 16:45:25 -0500

    Adds proper N making and input output streams for public and private key files

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 from Crypto.Util import Counter
 from Crypto import Random
 from Crypto import Util
-from math import gcd as bltin_gcd
+import argparse
 
 def inverse(a, n):
     a = a % n
@@ -13,20 +13,33 @@ def inverse(a, n):
     print('modular inverse does not exist')
     return 1
 
-def coprime2(a, b):
-    return bltin_gcd(a, b) == 1
+def find_gcd(a, b):
 
+    if a > b:
+        smallest = b
+    else:
+        smallest = a
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument('-k','--keyfile',required = True)
-# parser.add_argument('-i','--inputfile',required = True)
-# parser.add_argument('-o','--outputfile',required = True)
-# args = parser.parse_args()
+    for i in range(1, smallest + 1):
+        if((a % i == 0) and (b % i) == 0):
+            gcd = i
+
+    return gcd
+
+cmd = argparse.ArgumentParser()
+cmd.add_argument('-p','--publicKeyFile', required = True)
+cmd.add_argument('-s','--secretKeyFile', required = True)
+cmd.add_argument('-n','--bitNumber', required = True)
+args = cmd.parse_args()
 
 smallPrimeList = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
-p = Util.number.getPrime(10)
-q = Util.number.getPrime(10)
+publicOutput = args.publicKeyFile
+privateOutput = args.secretKeyFile
+n = int(args.bitNumber)
+
+p = Util.number.getPrime(int(n/2))
+q = Util.number.getPrime(int(n/2))
 
 print("p: " + str(p))
 print("q: " + str(q))
@@ -43,7 +56,7 @@ count = 0
 e = smallPrimeList[count]
 
 #find the smallest coprime to the order Zn
-while not coprime2(e, Zn):
+while not find_gcd(e, Zn) == 1:
     count += 1
     e = smallPrimeList[count]
 
@@ -55,5 +68,20 @@ print("d: " + str(d))
 
 print("public key = [N: " + str(N) + " e: " + str(e) + "]")
 print("private key = [N: " + str(N) + " d: " + str(d) + "]")
+
+#Open write file stream for public key
+out = open(publicOutput, "w")
+out.write(str(n) + "\n")
+out.write(str(N) + "\n")
+out.write(str(e))
+out.close()
+
+#Open write file stream for private key
+out = open(privateOutput, "w")
+out.write(str(n) + "\n")
+out.write(str(N) + "\n")
+out.write(str(d))
+out.close()
+
 
 
