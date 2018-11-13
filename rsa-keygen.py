bb400feb72f5a9eb2514188dae0e252ee6212998
mlwhitmer <whitmerlm@gmail.com>
2018-11-13 16:19:25 -0500

    Reworks inverse code

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -1,8 +1,8 @@
 from Crypto.Cipher import AES
-from Crypto.Util import Counter
-from Crypto import Random
 from Crypto import Util
 import argparse
+
+PRIME_LIST = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
 def inverse(a, n):
     a = a % n
@@ -13,35 +13,24 @@ def inverse(a, n):
     print('modular inverse does not exist')
     return 1
 
-# def extended_euclidian():
-
-def modInverse(a, m):
-    m0 = m
+def inverse(a, b):
+    original_b = b
     y = 0
     x = 1
 
-    if (m == 1):
-        return 0
+    while not a == 1:
+        quotient = a // b
+        t = b
+        b = a % b
 
-    while (a > 1):
-        # q is quotient
-        q = a // m
-
-        t = m
-
-        # m is remainder now, process
-        # same as Euclid's algo
-        m = a % m
         a = t
         t = y
 
-        # Update x and y
-        y = x - q * y
+        y = x - (quotient * y)
         x = t
 
-        # Make x positive
-    if (x < 0):
-        x = x + m0
+    if x < 0:
+        x = x + original_b
 
     return x
 
@@ -53,7 +42,7 @@ def find_gcd(a, b):
         smallest = a
 
     for i in range(1, smallest + 1):
-        if((a % i == 0) and (b % i) == 0):
+        if(a % i == 0) and (b % i) == 0:
             gcd = i
 
     return gcd
@@ -63,8 +52,6 @@ cmd.add_argument('-p','--publicKeyFile', required = True)
 cmd.add_argument('-s','--secretKeyFile', required = True)
 cmd.add_argument('-n','--bitNumber', required = True)
 args = cmd.parse_args()
-
-smallPrimeList = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
 publicOutput = args.publicKeyFile
 privateOutput = args.secretKeyFile
@@ -85,21 +72,21 @@ Zn = (p-1)*(q-1)
 print("Zn: " + str(Zn))
 
 count = 0
-e = smallPrimeList[count]
+e = PRIME_LIST[count]
 
 #find the smallest coprime to the order Zn
 while not find_gcd(e, Zn) == 1:
     count += 1
-    e = smallPrimeList[count]
+    e = PRIME_LIST[count]
 
 print("e: " + str(e))
 
-d_calc= modInverse(e, Zn)
+d = inverse(e, Zn)
 
-print("d_calc: " + str(d_calc))
+print("d: " + str(d))
 
 print("public key = [N: " + str(N) + " e: " + str(e) + "]")
-print("private key = [N: " + str(N) + " d: " + str(d_calc) + "]")
+print("private key = [N: " + str(N) + " d: " + str(d) + "]")
 
 #Open write file stream for public key
 out = open(publicOutput, "w")
@@ -112,7 +99,7 @@ out.close()
 out = open(privateOutput, "w")
 out.write(str(n) + "\n")
 out.write(str(N) + "\n")
-out.write(str(d_calc))
+out.write(str(d))
 out.close()
 
 
