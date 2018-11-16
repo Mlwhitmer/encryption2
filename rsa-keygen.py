eda143e288b3a9ef18c2024651d6c69512a3b1c1
mlwhitmer <whitmerlm@gmail.com>
2018-11-16 08:07:07 -0500

    Fixes keygen

--- rsa-keygen.py
+++ rsa-keygen.py
@@ -5,15 +5,6 @@ import math
 
 PRIME_LIST = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
-def inverse(a, n):
-    a = a % n
-    for x in range(1, n):
-        if ((a * x) % n == 1):
-            return x
-
-    print('modular inverse does not exist')
-    return 1
-
 def inverse(a, b):
     original_b = b
     y = 0
